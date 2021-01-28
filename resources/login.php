c0ac963c74f1e9ad78566395b91e403b8e7057a8
Andrew Querol <aquerol@percipia.com>
2021-01-28 15:18:24 -0600

    Allow redirecting to the path that was attempted before the login prompt.

--- resources/login.php
+++ resources/login.php
@@ -276,6 +276,9 @@
 
 		echo "<div id='login_form'>\n";
 		echo "<form name='login' method='post' action='".$_SESSION['login']['destination']['url']."'>\n";
+		if (isset($_REQUEST['path'])) {
+			echo "<input type='hidden' name='path' value='{$_REQUEST['path']}'/>\n";
+		}
 		echo "<input type='text' class='txt login' style='text-align: center; min-width: 200px; width: 200px; margin-bottom: 8px;' name='username' id='username' placeholder=\"".$text['label-username']."\"><br />\n";
 		echo "<input type='password' class='txt login' style='text-align: center; min-width: 200px; width: 200px; margin-bottom: 8px;' name='password' placeholder=\"".$text['label-password']."\"><br />\n";
 		if ($_SESSION['login']['domain_name_visible']['boolean'] == "true") {
